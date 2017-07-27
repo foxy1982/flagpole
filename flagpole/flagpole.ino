99dd7e5fad36d19d49e30c694405d63957f581fb
foxy1982 <daniel.fox@ukgateway.net>
2017-07-27 21:52:10 +0100

    updating main flagpole

--- flagpole/flagpole.ino
+++ flagpole/flagpole.ino
@@ -9,7 +9,7 @@ const uint16_t PixelCount = 90;
 const uint8_t PixelPin = 6;  // make sure to set this to the correct pin, ignored for Esp8266
 
 HslColor _colourList[] = { hslRed, hslYellow, hslBlue };
-const uint8_t ColourCount = 6;
+const uint8_t ColourCount = 3;
 
 // three element pixels, in different order and speeds
 NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> _strip(PixelCount, PixelPin);
@@ -23,7 +23,7 @@ void setup()
 
 void loop()
 {
-    //SimpleChange(_strip, _colourList, ColourCount);
-    //FlashColours(_strip, _colourList, ColourCount, hslBlack);
+    SimpleChange(_strip, _colourList, ColourCount);
+    FlashColours(_strip, _colourList, ColourCount, hslBlack);
     FillWithStripes(_strip, _colourList, ColourCount, hslBlack, PixelCount);
 }
